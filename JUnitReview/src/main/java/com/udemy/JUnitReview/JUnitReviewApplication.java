54670dc5646030c3ce66eab453ad92d324fecd41
Raphael ANDRE MATIAS <raphaelam.pro@gmail.com>
2023-04-01 15:17:03 +0200

    init

---
+++ JUnitReview/src/main/java/com/udemy/JUnitReview/JUnitReviewApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.JUnitReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUnitReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JUnitReviewApplication.class, args);
+	}
+
+}
---
+++ JUnitReview/src/test/java/com/udemy/JUnitReview/JUnitReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.JUnitReview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnitReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
