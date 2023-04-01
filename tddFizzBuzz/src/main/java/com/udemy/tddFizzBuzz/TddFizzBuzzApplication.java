462cd994e8890b81a690006a269949a24fe314ce
Raphael ANDRE MATIAS <raphaelam.pro@gmail.com>
2023-04-01 22:49:09 +0200

    init

---
+++ tddFizzBuzz/src/main/java/com/udemy/tddFizzBuzz/TddFizzBuzzApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.tddFizzBuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddFizzBuzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddFizzBuzzApplication.class, args);
+	}
+
+}
